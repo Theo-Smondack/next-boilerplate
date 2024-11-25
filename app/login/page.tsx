4e6a9a20ee73b1f3b38570e7f6015d9ceea0ca60
Theo-Smondack <smondack.theo.pro@gmail.com>
2024-11-25 23:40:51 +0100

    Fix responsive login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 const LoginPage = () => {
     return (
         <div className="flex h-screen">
-            <Card className="m-auto w-1/6">
+            <Card className="m-auto lg:w-1/4 2xl:w-1/6 md:w-1/3 w-9/12">
                 <CardHeader>
                     <CardTitle>Login</CardTitle>
                 </CardHeader>
